31de403188fb757351fad25a7d150da737ba2dfc
Vandad Nahavandipoor <vandad@Vandads-MacBook-Pro.local>
2020-06-02 20:34:15 +0200

    project structure

---
+++ chords/README.md
@@ -0,0 +1,16 @@
+# chords
+
+An app that plays an audio of a guitar chord and you'll have to guess if it's a major or a minor chord
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
+
+For help getting started with Flutter, view our
+[online documentation](https://flutter.dev/docs), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
---
+++ chords/lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData.dark(),
+      home: WelcomePage(),
+    );
+  }
+}
+
+class WelcomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Chords'),
+      ),
+      body: Center(
+          child: RaisedButton(
+        child: Text(
+          'READY?',
+          style: TextStyle(
+            fontSize: 30.0
+          ),
+        ),
+        elevation: 5.0,
+        color: Colors.lightBlueAccent,
+        padding: EdgeInsets.symmetric(
+          horizontal: 20,
+          vertical: 10,
+        ),
+        onPressed: () => {},
+      )),
+    );
+  }
+}
